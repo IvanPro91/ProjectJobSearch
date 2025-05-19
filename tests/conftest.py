d62b907c5e6770c3a643c19e1773b40512cd0377
IvanPro91 <ivan-don4encko@yandex.ru>
2025-05-19 20:18:39 +0300

    Обновлены фикстуры

--- tests/conftest.py
+++ tests/conftest.py
@@ -109,6 +109,8 @@ def init_vacancy() -> Vacancy:
         name="Тестировщик (QA-инженер)",
         url="https://api.hh.ru/vacancies/119602010?host=hh.ru",
         salary_from=1,
+        company_name="name",
+        company_id=1,
         salary_to=10000,
         requirement="Знакомство с Postman и умение составлять API запросы. Знание основ HTML, CSS, JS, "
         "<highlighttext>Python</highlighttext>. Опыт написания тестовой документации "
